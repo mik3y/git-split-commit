61279d95854a92b34bb4ee5320c7ab5a4716ef5c
mike wakerly <opensource@hoho.com>
2020-05-22 13:56:01 -0400

    Python package updates.

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
-from setuptools import setup
-setup(setup_requires=["pbr"], pbr=True)
+from setuptools import setup, find_packages
+
+setup(
+    name="git_split_commit",
+    packages=find_packages(),
+    version="0.1.0",
+    install_requires=["click", "coloredlogs", "gitpython", "simple-term-menu"],
+    entry_points={"console_scripts": ["git-split-commit = git_split_commit.command:split"]},
+)
